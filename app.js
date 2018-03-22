332e8858734f1ef412ac27ce1ac121c19f5d55b2
Adrian <adcaymo@gmail.com>
2018-03-21 19:52:31 -0700

    indent clean up

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express 				= require('express'),
-		app 						= express(),
-		bodyParser 			= require('body-parser'),
-		mongoose 				= require('mongoose'),
-		flash						= require('connect-flash'),
-		passport				= require('passport'),
-		LocalStrategy		= require('passport-local'),
-		methodOverride	= require('method-override'),
-		Campground 			= require('./models/campground'),
-		Comment 				= require('./models/comment'),
-		User						= require('./models/user');
+var express	= require('express'),
+		app	= express(),
+		bodyParser = require('body-parser'),
+		mongoose = require('mongoose'),
+		flash = require('connect-flash'),
+		passport = require('passport'),
+		LocalStrategy = require('passport-local'),
+		methodOverride = require('method-override'),
+		Campground = require('./models/campground'),
+		Comment	= require('./models/comment'),
+		User = require('./models/user');
 
 //requiring routes
 var commentRoutes 		= require('./routes/comments'),
