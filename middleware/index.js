1d3ed4ad876ab3375480f46ef0990f63ba7bf10e
Adrian <adcaymo@gmail.com>
2018-03-21 19:33:41 -0700

    minor code cleanup

--- middleware/index.js
+++ middleware/index.js
@@ -48,10 +48,9 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
 middlewareObj.isLoggedIn = function(req, res, next){
 	if (req.isAuthenticated()) {
 		return next();
-	} else {
-		req.flash("error", "You need to be logged in to do that.");
-		res.redirect("/login");
 	}
+	req.flash("error", "You need to be logged in to do that.");
+	res.redirect("/login");	
 }
 
 module.exports = middlewareObj;
