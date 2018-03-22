9cf676f78c6498fb05197a89c011b51bbd1a25d4
Adrian <adcaymo@gmail.com>
2018-03-21 19:56:54 -0700

    minor code cleanup

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var express	= require('express'),
-		app	= express(),
-		bodyParser = require('body-parser'),
-		mongoose = require('mongoose'),
-		flash = require('connect-flash'),
-		passport = require('passport'),
-		LocalStrategy = require('passport-local'),
-		methodOverride = require('method-override'),
-		Campground = require('./models/campground'),
-		Comment	= require('./models/comment'),
-		User = require('./models/user');
+app	= express(),
+bodyParser = require('body-parser'),
+mongoose = require('mongoose'),
+flash = require('connect-flash'),
+passport = require('passport'),
+LocalStrategy = require('passport-local'),
+methodOverride = require('method-override'),
+Campground = require('./models/campground'),
+Comment	= require('./models/comment'),
+User = require('./models/user');
 
 //requiring routes
-var commentRoutes 		= require('./routes/comments'),
-		campgroundRoutes	= require('./routes/campgrounds'),
-		indexRoutes				= require('./routes/index');
+var commentRoutes	= require('./routes/comments'),
+campgroundRoutes = require('./routes/campgrounds'),
+indexRoutes	= require('./routes/index');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASEURL, {useMongoClient: true});
