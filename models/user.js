369d72e3b7b5f3bc94a732fc58596be94d28d637
Adrian <adcaymo@gmail.com>
2018-03-22 19:23:14 -0700

    add user profile feature

--- models/user.js
+++ models/user.js
@@ -3,7 +3,11 @@ var mongoose              = require('mongoose'),
 
 var userSchema = new mongoose.Schema({
   username: String,
-  password: String
+  password: String,
+  avatar: String,
+  firstName: String,
+  lastName: String,
+  email: String
 }, {usePushEach: true});
 
 userSchema.plugin(passportLocalMongoose);
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,28 @@
-var express  = require('express'),
-    router   = express.Router(),
-    passport = require('passport'),
-    User     = require('../models/user');
+var express    = require('express'),
+    router     = express.Router(),
+    passport   = require('passport'),
+    User       = require('../models/user'),
+    Campground = require('../models/campground');
 
-//ROOT ROUTE
+// ROOT ROUTE
 router.get("/", function(req, res) {
   res.render("landing");
 });
 
-//REGISTER FORM ROUTE - show sign up form
+// REGISTER FORM ROUTE - show sign up form
 router.get("/register", function(req, res) {
   res.render("register", {page: 'register'});
 });
 
-//SIGN UP LOGIC ROUTE - handle sign up logic
+// SIGN UP LOGIC ROUTE - handle sign up logic
 router.post("/register", function(req, res) {
-  var newUser = new User({username: req.body.username});
+  var newUser = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    avatar: req.body.avatar,
+    email: req.body.email
+  });
   User.register(newUser, req.body.password, function(err, user) {
     if (err) {
       console.log(err);
@@ -28,12 +35,12 @@ router.post("/register", function(req, res) {
   });
 });
 
-//LOGIN FORM ROUTE - show login form
+// LOGIN FORM ROUTE - show login form
 router.get("/login", function(req, res) {
   res.render("login", {page: 'login'});
 });
 
-//LOGIN LOGIC ROUTE - handle login logic
+// LOGIN LOGIC ROUTE - handle login logic
 router.post("/login", passport.authenticate("local",
   {
     successRedirect: "/campgrounds",
@@ -41,11 +48,30 @@ router.post("/login", passport.authenticate("local",
   }), function(req, res) {
 });
 
-//LOGOUT ROUTE - logout user
+// LOGOUT ROUTE - logout user
 router.get("/logout", function(req, res) {
   req.logout();
   req.flash("success", "You have been logged out.");
   res.redirect("/campgrounds");
+});
+
+// USER PROFILE ROUTE
+router.get("/users/:id", function(req, res) {
+  User.findById(req.params.id, function(err, foundUser) {
+    if (err) {
+      req.flash("error", "User not found.");
+      res.redirect("/campgrounds");
+    } else {
+      Campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds) {
+        if (err) {
+          req.flash("error", "Campgrounds not found.");
+          res.redirect("/campgrounds");
+        } else {
+          res.render("users/show", {user: foundUser, campgrounds: campgrounds});  
+        }
+      })
+    }
+  });
 });
 
 module.exports = router;
